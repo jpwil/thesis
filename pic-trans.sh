4fa386cdf7d1d0ed69e4043b4a8f40db3822fc62
James Wilson <jpwilson123@outlook.com>
2025-11-26 07:18:21 +0000

    results descriptive figure updates

---
+++ pic-trans.sh
@@ -0,0 +1,34 @@
+# chapter 5 - results
+
+# combo pooled distributions
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/age_comb.pdf figures/ch5/isc_pool_age_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/height_comb.pdf figures/ch5/isc_pool_height_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/weight_comb.pdf figures/ch5/isc_pool_weight_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/ss_comb.pdf figures/ch5/isc_pool_ss_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/fd_comb.pdf figures/ch5/isc_pool_fd_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/wbc_comb.pdf figures/ch5/isc_pool_wbc_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/plt_comb.pdf figures/ch5/isc_pool_plt_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/hb_comb.pdf figures/ch5/isc_pool_hb_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/alt_comb.pdf figures/ch5/isc_pool_alt_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/cr_comb.pdf figures/ch5/isc_pool_cr_comb.pdf
+
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/catOut/comb_cat.pdf figures/ch5/isc_cat_comb.pdf
+
+# by study figure
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/main_dist.pdf figures/ch5/isc_main_dist.pdf
+
+# APPENDIX - chapter 5
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/age_comb.pdf figures/ch5/isc_age_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/height_comb.pdf figures/ch5/isc_height_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/weight_comb.pdf figures/ch5/isc_weight_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/bmi_comb.pdf figures/ch5/isc_bmi_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/bmiz_comb.pdf figures/ch5/isc_bmiz_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/wfh_comb.pdf figures/ch5/isc_wfh_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/ss_comb.pdf figures/ch5/isc_ss_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/fd_log_comb.pdf figures/ch5/isc_fd_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/wbc_log_comb.pdf figures/ch5/isc_wbc_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/plt_log_comb.pdf figures/ch5/isc_plt_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/hb_log_comb.pdf figures/ch5/isc_hb_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/alt_log_comb.pdf figures/ch5/isc_alt_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/cr_log_comb.pdf figures/ch5/isc_cr_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/comb_discrete.pdf figures/ch5/isc_comb_discrete.pdf
