4ae20576ee0eb2eae6a3930f4cbcb0212794f680
James Wilson <jpwilson123@outlook.com>
2025-11-27 16:50:25 +0000

    Last of the ISC descriptive figures. Off to Tenerife

--- pic-trans.sh
+++ pic-trans.sh
@@ -1,23 +1,28 @@
-# chapter 5 - results
+# CHAPTER 5 FIGURES
 
-# combo pooled distributions
+# combo pooled continuous distributions - non-lab
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/age_comb.pdf figures/ch5/isc_pool_age_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/height_comb.pdf figures/ch5/isc_pool_height_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/weight_comb.pdf figures/ch5/isc_pool_weight_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/ss_comb.pdf figures/ch5/isc_pool_ss_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/fd_comb.pdf figures/ch5/isc_pool_fd_comb.pdf
+
+# combo pooled continuous distributions - lab
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/wbc_comb.pdf figures/ch5/isc_pool_wbc_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/plt_comb.pdf figures/ch5/isc_pool_plt_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/hb_comb.pdf figures/ch5/isc_pool_hb_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/alt_comb.pdf figures/ch5/isc_pool_alt_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/contOut/cr_comb.pdf figures/ch5/isc_pool_cr_comb.pdf
 
+# combo pooled categorical distributions
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/catOut/comb_cat.pdf figures/ch5/isc_cat_comb.pdf
 
-# by study figure
+# selected study-specific distributions (study n / relapse / sex/ age)
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/main_dist.pdf figures/ch5/isc_main_dist.pdf
 
-# APPENDIX - chapter 5
+# APPENDIX - CHAPTER 5
+
+# continuous study-specific distributions (including age-dependent malnutrition markers)
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/age_comb.pdf figures/ch5/isc_age_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/height_comb.pdf figures/ch5/isc_height_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/weight_comb.pdf figures/ch5/isc_weight_comb.pdf
@@ -31,4 +36,12 @@ cp /Users/jameswilson/proj/vl_model_isc/figures/dist/plt_log_comb.pdf figures/ch
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/hb_log_comb.pdf figures/ch5/isc_hb_log_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/alt_log_comb.pdf figures/ch5/isc_alt_log_comb.pdf
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/cr_log_comb.pdf figures/ch5/isc_cr_log_comb.pdf
+
+# remaining study-specific distributions
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/comb_discrete.pdf figures/ch5/isc_comb_discrete.pdf
+
+# associations - cont-cont
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/ass/cont_cont.pdf figures/ch5/isc_cont_cont.pdf
+
+# associations - cont-cat
+cp /Users/jameswilson/proj/vl_model_isc/figures/dist/ass/cont_cat.pdf figures/ch5/isc_cont_cat.pdf
