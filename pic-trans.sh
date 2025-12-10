d64069447df86278d243264ad6ea539b884e458c
James Wilson <jpwilson123@outlook.com>
2025-12-10 09:45:09 +0000

    Completed descriptive results for EA

--- pic-trans.sh
+++ pic-trans.sh
@@ -20,6 +20,9 @@ cp /Users/jameswilson/proj/vl_model_isc/figures/dist/catOut/comb_cat.pdf figures
 # selected study-specific distributions (study n / relapse / sex/ age)
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/main_dist.pdf figures/ch5/isc_main_dist.pdf
 
+# missing data summary
+cp /Users/jameswilson/proj/vl_model_isc/figures/missing/summary.pdf figures/ch5/isc_missing_summary.pdf
+
 # APPENDIX - CHAPTER 5
 
 # continuous study-specific distributions (including age-dependent malnutrition markers)
@@ -45,3 +48,55 @@ cp /Users/jameswilson/proj/vl_model_isc/figures/dist/ass/cont_cont.pdf figures/c
 
 # associations - cont-cat
 cp /Users/jameswilson/proj/vl_model_isc/figures/dist/ass/cont_cat.pdf figures/ch5/isc_cont_cat.pdf
+
+# CHAPTER 6 FIGURES
+
+# combo pooled continuous distributions - non-lab
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/age_comb.pdf figures/ch6/ea_pool_age_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/height_comb.pdf figures/ch6/ea_pool_height_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/weight_comb.pdf figures/ch6/ea_pool_weight_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/ss_comb.pdf figures/ch6/ea_pool_ss_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/fd_comb.pdf figures/ch6/ea_pool_fd_comb.pdf
+
+# combo pooled continuous distributions - lab
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/wbc_comb.pdf figures/ch6/ea_pool_wbc_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/plt_comb.pdf figures/ch6/ea_pool_plt_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/hb_comb.pdf figures/ch6/ea_pool_hb_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/alt_comb.pdf figures/ch6/ea_pool_alt_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/contOut/cr_comb.pdf figures/ch6/ea_pool_cr_comb.pdf
+
+# combo pooled categorical distributions
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/catOut/comb_cat.pdf figures/ch6/ea_cat_comb.pdf
+
+# selected study-specific distributions (study n / relapse / sex/ age)
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/main_dist.pdf figures/ch6/ea_main_dist.pdf
+
+# missing data summary
+cp /Users/jameswilson/proj/vl_model_ea/figures/missing/summary.pdf figures/ch6/ea_missing_summary.pdf
+
+
+# APPENDIX - CHAPTER 6
+
+# continuous study-specific distributions (including age-dependent malnutrition markers)
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/age_comb.pdf figures/ch6/ea_age_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/height_comb.pdf figures/ch6/ea_height_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/weight_comb.pdf figures/ch6/ea_weight_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/bmi_comb.pdf figures/ch6/ea_bmi_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/bmiz_comb.pdf figures/ch6/ea_bmiz_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/wfh_comb.pdf figures/ch6/ea_wfh_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/ss_comb.pdf figures/ch6/ea_ss_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/fd_log_comb.pdf figures/ch6/ea_fd_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/wbc_log_comb.pdf figures/ch6/ea_wbc_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/plt_log_comb.pdf figures/ch6/ea_plt_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/hb_log_comb.pdf figures/ch6/ea_hb_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/alt_log_comb.pdf figures/ch6/ea_alt_log_comb.pdf
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/cr_log_comb.pdf figures/ch6/ea_cr_log_comb.pdf
+
+# remaining study-specific distributions
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/comb_discrete.pdf figures/ch6/ea_comb_discrete.pdf
+
+# # associations - cont-cont
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/ass/cont_cont.pdf figures/ch6/ea_cont_cont.pdf
+
+# # associations - cont-cat
+cp /Users/jameswilson/proj/vl_model_ea/figures/dist/ass/cont_cat.pdf figures/ch6/ea_cont_cat.pdf
